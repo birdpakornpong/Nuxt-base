755e52040a5eb329f54a8a086e1202e02e0bb6da
KHUNBIRD <birdpakornpong@gmail.com>
2021-10-23 03:24:55 +0700

    text

---
+++ layouts/admin.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav
+      class="navbar header has-shadow is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          <img
+            src="~assets/buefy.png"
+            alt="Buefy"
+            height="28"
+          >
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+      <aside class="column is-3 section">
+        <Menu />
+      </aside>
+
+      <div class="container column is-9">
+        <Nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Menu from '~/components/Menu'
+export default {
+  components: {
+    Menu
+  },
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <h2 class="title is-3 has-text-grey">
+      "Just start  <b-icon
+        icon="rocket"
+        size="is-large"
+      />"
+    </h2>
+    <h3 class="subtitle is-6 has-text-grey">
+      Author: <a href="https://github.com/anteriovieira">
+        Antério Vieira
+      </a>
+    </h3>
+    <Form title="Form"></Form>
+  </section>
+</template>
+
+
+<script>
+import Form from '~/components/Form'
+
+export default {
+  name: 'Inspire',
+  components: {
+    Form
+  },
+  layout: "admin",
+}
+</script>
